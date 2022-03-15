6512ab50204ae10d8751ae07be0c5443cc217d26
The Mafia <issactoluwanimi@gmail.com>
2022-03-15 08:15:47 -0700

    Adding new File(java)

---
+++ newfile.java
@@ -0,0 +1,12 @@
+class newfile{
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
+        int r;
+        System.out.println("Hello We Are using git version control system");
+    }
+        
+        
+    
+}
